518065c934b567788d17670344d3c8f93412d446
woodwardtw <twwoodward@me.com>
2022-06-24 15:13:36 -0400

    modal integration

--- loop-templates/content-single-student.php
+++ loop-templates/content-single-student.php
@@ -23,10 +23,25 @@ defined( 'ABSPATH' ) || exit;
 		the_content();
 		pp_practice_log();
 		?>
-
+		<button id="logButton" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#logData">
+		  Add practice
+		</button>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
+		<!-- Modal -->
+		<div class="modal fade" id="logData" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div class="modal-dialog modal-xl">
+		    <div class="modal-content">
+		      <div class="modal-body">
+		       <?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true"]')?>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 
 		<?php understrap_entry_footer(); ?>
 
