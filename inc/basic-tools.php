366c4018d22d34f0a5988c1ddb67c82b453b596f
woodwardtw <twwoodward@me.com>
2022-06-28 12:43:37 -0400

    initial redirect
    covers if not logged in
    covers if student tries to go elsewhere
    
    need to redirect from dashboard

--- inc/basic-tools.php
+++ inc/basic-tools.php
@@ -29,3 +29,43 @@ function pp_make_students(){
     }
 }
 
+
+//redirections
+add_action('template_redirect', 'pp_user_redirection');
+
+function pp_user_redirection(){
+    global $post;
+    $url = get_site_url();
+    if(!is_user_logged_in()){
+        wp_redirect($url . '/wp-login.php'); 
+        exit;
+    }
+    if(is_user_logged_in()){
+        $user_id = get_current_user_id();
+        if(pp_user_has_role($user_id, 'p_student')){
+            $slug = wp_get_current_user()->user_login;
+            if($post->post_name != $slug){
+                wp_redirect($url . '/' . $slug); 
+                exit;
+            }
+        }
+    }
+}
+
+
+//user registration set slug
+add_action( 'gform_advancedpostcreation_post_after_creation', 'pp_set_student_slug', 10, 2 );
+
+function pp_set_student_slug($post_id, $feed, $entry, $form){
+    wp_update_post([
+      "post_name" => $entry[7],
+      "ID" => $post_id,
+    ]);
+}
+
+
+function pp_user_has_role($user_id, $role_name){
+        $user_meta = get_userdata($user_id);
+        $user_roles = $user_meta->roles;
+        return in_array($role_name, $user_roles);
+    }
