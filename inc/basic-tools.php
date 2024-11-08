baa3e4484539be7a5be36f64791166cf78d5dd77
woodwardtw <twwoodward@me.com>
2024-11-08 10:28:28 -0500

    test

--- inc/basic-tools.php
+++ inc/basic-tools.php
@@ -46,12 +46,16 @@ function pp_user_redirection(){
         $first = wp_get_current_user()->user_firstname;
         $last = wp_get_current_user()->user_lastname;
         $slug = strtolower($last . '-' . $first);
-        if(pp_user_has_role($user_id, 'p_student')){
-            if($post->post_name != $slug && $current_url != $url . '/student/' . $slug){
-                wp_redirect($url . '/student/' . $slug); 
-                exit;
-            }
-        }
+        var_dump($slug);
+        var_dump(pp_user_has_role($user_id, 'p_student'));
+        var_dump($url . '/student/' . $slug);
+
+        // if(pp_user_has_role($user_id, 'p_student')){
+        //     if($post->post_name != $slug && $current_url != $url . '/student/' . $slug){
+        //         wp_redirect($url . '/student/' . $slug); 
+        //         exit;
+        //     }
+        // }
     }
 }
 
